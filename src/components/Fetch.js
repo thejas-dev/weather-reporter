f28999cf3efca52f4f42c9d5fee4636cc19931d4
thejas <thejaskala308@gmail.com>
2022-08-03 14:00:42 +0530

    make it better

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -82,7 +82,7 @@ useEffect(()=>{
 		.then(res => res.json())
 		.then(result => {
 			setInfo(result)
-			if(result.main.temp>=0 && result.main.temp<=10){
+			if(result.main.temp<=10){
 				setBg('body-snow')
 			}
 			if(result.main.temp>=10 && result.main.temp<=20){
@@ -103,11 +103,24 @@ useEffect(()=>{
 			if(result.main.temp>=45 && result.main.temp<=60){
 				setBg('body-vhot2')
 			}
-			if( result.weather[0].description==='mist'){
+			if( result.weather[0].description==='Mist' || result.weather[0].description==='Smoke' || result.weather[0].description==='Haze' || result.weather[0].description==='Ash'
+			 || result.weather[0].description==='Dust' || result.weather[0].description==='Fog' || result.weather[0].description==='Sand' || result.weather[0].description==='Squall'
+			  || result.weather[0].description==='Tornado' ){
 				setBg('body-mist')
 			}
-			if(result.main.humidity>90 || result.weather[0].description==='heavy intensity rain' ){
+			if(result.main.humidity>90 || result.weather[0].description==='heavy intensity rain' || result.weather[0].description==='light rain' || result.weather[0].description==='moderate rain'
+				|| result.weather[0].description==='very heavy rain'|| result.weather[0].description==='extreme rain'|| result.weather[0].description==='freezing rain' || result.weather[0].description==='light intensity shower rain'
+				 || result.weather[0].description==='shower rain' || result.weather[0].description==='ragged shower rain' || result.weather[0].description==='heavy intensity shower rain'){
 				setBg('body-rain')
+			}
+			if(result.weather[0].description==='drizzle' || result.weather[0].description==='light intensity drizzle' || result.weather[0].description==='drizzle rain' || result.weather[0].description==='heavy intensity drizzle'
+				 || result.weather[0].description==='light intensity drizzle rain' || result.weather[0].description==='shower rain and drizzle' || result.weather[0].description==='heavy shower rain and drizzle' || result.weather[0].description==='shower drizzle'){
+				setBg('body-drizzle')
+			}
+			if(result.weather[0].description==='thunderstorm' || result.weather[0].description==='thunderstorm with light rain' || result.weather[0].description==='thunderstorm with rain' || result.weather[0].description==='thunderstorm with heavy rain' || result.weather[0].description==='light thunderstorm'
+				 || result.weather[0].description==='heavy thunderstorm' || result.weather[0].description==='ragged thunderstorm' || result.weather[0].description==='thunderstorm with light drizzle'
+				  || result.weather[0].description==='thunderstorm with drizzle' || result.weather[0].description==='thunderstorm with heavy drizzle'){
+				setBg('body-thunder')
 			}
 			
 		});
@@ -154,7 +167,7 @@ useEffect(()=>{
 			.then(res => res.json())
 			.then(result => {
 			setInfo(result)	
-			if(result.main.temp>=0 && result.main.temp<=10){
+			if(result.main.temp<=10){
 				setBg('body-snow')
 			}
 			if(result.main.temp>=10 && result.main.temp<=20){
@@ -175,11 +188,24 @@ useEffect(()=>{
 			if(result.main.temp>=45 && result.main.temp<=60){
 				setBg('body-vhot2')
 			}
-			if( result.weather[0].description==='mist'){
+			if( result.weather[0].description==='Mist' || result.weather[0].description==='Smoke' || result.weather[0].description==='Haze' || result.weather[0].description==='Ash'
+			 || result.weather[0].description==='Dust' || result.weather[0].description==='Fog' || result.weather[0].description==='Sand' || result.weather[0].description==='Squall'
+			  || result.weather[0].description==='Tornado' ){
 				setBg('body-mist')
 			}
-			if(result.main.humidity>90 || result.weather[0].description==='heavy intensity rain' ){
+			if(result.main.humidity>90 || result.weather[0].description==='heavy intensity rain' || result.weather[0].description==='light rain' || result.weather[0].description==='moderate rain'
+				|| result.weather[0].description==='very heavy rain'|| result.weather[0].description==='extreme rain'|| result.weather[0].description==='freezing rain' || result.weather[0].description==='light intensity shower rain'
+				 || result.weather[0].description==='shower rain' || result.weather[0].description==='ragged shower rain' || result.weather[0].description==='heavy intensity shower rain'){
 				setBg('body-rain')
+			}
+			if(result.weather[0].description==='drizzle' || result.weather[0].description==='light intensity drizzle' || result.weather[0].description==='drizzle rain' || result.weather[0].description==='heavy intensity drizzle'
+				 || result.weather[0].description==='light intensity drizzle rain' || result.weather[0].description==='shower rain and drizzle' || result.weather[0].description==='heavy shower rain and drizzle' || result.weather[0].description==='shower drizzle'){
+				setBg('body-drizzle')
+			}
+			if(result.weather[0].description==='thunderstorm' || result.weather[0].description==='thunderstorm with light rain' || result.weather[0].description==='thunderstorm with rain' || result.weather[0].description==='thunderstorm with heavy rain' || result.weather[0].description==='light thunderstorm'
+				 || result.weather[0].description==='heavy thunderstorm' || result.weather[0].description==='ragged thunderstorm' || result.weather[0].description==='thunderstorm with light drizzle'
+				  || result.weather[0].description==='thunderstorm with drizzle' || result.weather[0].description==='thunderstorm with heavy drizzle'){
+				setBg('body-thunder')
 			}
 		})
 			setLat(null)
